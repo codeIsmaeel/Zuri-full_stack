303449babb54db8875584c384c9e2847b0b25322
codeIsmaeel <debolaqasim@gmail.com>
2022-06-15 15:57:54 +0100

    code correction

--- Python/anagram.py
+++ Python/anagram.py
@@ -1,10 +1,26 @@
-def find_anagram(word, anagram):
-    # [assignment] Add your code here
+# def find_anagram(word, anagram):
+#     # [assignment] Add your code here
 
-    word = input('enter word: ')
-    anagram = input('enter anagram: ')
+#     word = input('enter word: ')
+#     anagram = input('enter anagram: ')
+
+#     return sorted(word) == sorted(anagram)
+
+
+# print(find_anagram('word', 'anagram'))
+
+
+#  corrected solution to finding anagrams task
+
+def find_anagram(word, anagram):
+
+    word = input("Enter word: ")
+    anagram = input("Enter anagram: ")
+
+    word = word.lower() .replace(" ", "") .strip()
+    anagram = anagram.lower() .replace(" ", "") .strip()
 
     return sorted(word) == sorted(anagram)
 
 
-print(find_anagram('word', 'anagram'))
+print(find_anagram("word", "anagram"))
