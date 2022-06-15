52544ee6bd44cc3acc65135dca068e5f7181b1e1
codeIsmaeel <debolaqasim@gmail.com>
2022-06-15 16:10:25 +0100

    udate repo

--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,8 +1,7 @@
-Read text from a file, and count the occurence of words in that text
+
 # Example:
 # count_words("The cake is done. It is a big cake!")
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
-
 
 
 # def read_file_content(read-text):
